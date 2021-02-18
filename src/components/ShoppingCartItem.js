e1c5a1a0da6845a5851be89ab2a4303537ac4a54
Patrice Jean <73203091+PhatherJean@users.noreply.github.com>
2021-02-18 18:46:20 -0500

    did one of the stretches

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useContext } from "react";
+// context
+import { CartContext } from "../context";
 
-const Item = props => {
-	return (
-		<div className="shopping-cart_item">
-			<img src={props.image} alt={`${props.title} book`} />
+const Item = (props) => {
+  const { cart, setCart } = useContext(CartContext);
 
+  console.log(cart);
+  const handleRemove = (id) => {
+    setCart(
+      cart.filter((item) => {
+        console.log(item);
+        return id !== item.id;
+      })
+    );
+  };
+  return (
+    <div className="shopping-cart_item">
+      <img src={props.image} alt={`${props.title} book`} />
 
-			<div>
-				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
-			</div>
-		</div>
-	);
+      <div>
+        <h1>{props.title}</h1>
+        <p>$ {props.price}</p>
+        <button onClick={() => handleRemove(props.id)}>Remove from cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
